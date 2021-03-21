a78f776cb027cd4493ecdfcf7019b397c75ebbb3
Cristian Botez <cbotez22@gmail.com>
2021-03-21 16:41:10 +0200

    Add font color & size props for the Logo component

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { logo } from "./../../utils/images";
 import PropTypes from "prop-types";
 
-function Logo({ styles }) {
+function Logo({ styles, fontColor, fontSize }) {
+  if (!fontColor) fontColor = "text-gray-100";
+  if (!fontSize) fontSize = "text-2xl tablet:text-3xl";
   return (
     <a className={`flex flex-row items-center space-x-3 ${styles}`} href="/">
       <img className="w-10 laptop:w-12" src={logo} alt="A pirate ship logo" />
-      <h1 className="text-gray-100 font-normal font-pirate-gr tracking-wider text-shadow-lg text-2xl tablet:text-3xl">
+      <h1
+        className={`${fontColor} ${fontSize} font-normal font-pirate-gr text-shadow-lg`}
+      >
         Shipirate VPN
       </h1>
     </a>
