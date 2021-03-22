dd427fa70a4d491f79b7d67e52f6192d45e5817f
Cristian Botez <cbotez22@gmail.com>
2021-03-22 16:05:26 +0200

    Add w-full and remove imports in Input component

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,6 +1,5 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { PropTypes } from "prop-types";
-import { Check } from "./Icons";
 
 function Input({ type, id, placeholder }) {
   return (
@@ -10,8 +9,8 @@ function Input({ type, id, placeholder }) {
       placeholder={placeholder}
       className={
         type === "checkbox"
-          ? "appearance-none cursor-pointer outline-none w-5 h-5 bg-no-repeat bg-checkbox-unchecked checked:bg-checkbox-checked rounded-lg"
-          : "outline-none text-lg rounded-xl px-4 py-2 focus:bg-gray-100 placeholder-blue-grayish text-blue-dark bg-gray-25"
+          ? "appearance-none cursor-pointer outline-none w-4 h-4 bg-no-repeat bg-checkbox-unchecked checked:bg-checkbox-checked rounded-lg"
+          : "outline-none text-lg rounded-xl px-4 py-2 focus:bg-gray-100 placeholder-blue-grayish text-blue-dark bg-gray-25 w-full"
       }
     />
   );
