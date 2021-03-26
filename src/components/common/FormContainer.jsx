f03bb53c08d5502a36f3c18e8d557553956349b1
Cristian Botez <cbotez22@gmail.com>
2021-03-26 13:24:17 +0200

    Clean a few classes in FormContainer component

--- src/components/common/FormContainer.jsx
+++ src/components/common/FormContainer.jsx
@@ -4,11 +4,11 @@ import { Cross } from "./Icons";
 
 function FormContainer({ children }) {
   return (
-    <div className="z-10 flex flex-row justify-center bg-white-faded-50 items-center fixed opacity-100 w-full h-full overflow-hidden">
+    <div className="z-10 flex flex-row justify-center bg-white-faded-50 items-center fixed opacity-100 h-full w-full">
       <div className="relative flex flex-col justify-center items-center p-12 text-blue-dark rounded-3xl shadow-xl bg-gradient-to-t from-blue-sky-2 to-blue-water max-w-xs">
         <Cross className="absolute w-5 fill-current m-4 top-0 right-0 cursor-pointer transition-colors hover:text-gray-25" />
         <Logo
-          styles="absolute -top-18 laptop:-top-20"
+          styles="absolute -top-16"
           fontColor="text-blue-dark"
           fontSize="text-3xl laptop:text-4xl"
         />
