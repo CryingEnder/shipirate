97f558ce6c4030e10f46280edb7199196f1930a3
Cristian Botez <cbotez22@gmail.com>
2021-03-26 13:20:49 +0200

    Make props come from ...props in Input component

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,28 +1,16 @@
 import React from "react";
-import { PropTypes } from "prop-types";
 
-function Input({ type, id, placeholder }) {
+function Input({ ...props }) {
   return (
     <input
-      type={type}
-      id={id}
-      placeholder={placeholder}
+      {...props}
       className={
-        type === "checkbox"
+        props.type === "checkbox" || props.type === "radio"
           ? "appearance-none cursor-pointer outline-none w-4 h-4 bg-no-repeat bg-checkbox-unchecked checked:bg-checkbox-checked rounded-lg"
           : "outline-none text-lg rounded-xl px-4 py-2 focus:bg-gray-100 placeholder-blue-grayish text-blue-dark bg-gray-25 w-full"
       }
     />
   );
 }
-
-Input.defaultProps = {
-  id: "",
-  placeholder: "",
-};
-
-Input.propTypes = {
-  label: PropTypes.string,
-};
 
 export default Input;
