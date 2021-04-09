faefb2fc881c9b6f0bf65909d2a8cd4f32b7193d
Cristian Botez <cbotez22@gmail.com>
2021-04-09 16:14:11 +0300

    Add logo size utility for phones

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import { logo } from "./../../utils/images";
 import PropTypes from "prop-types";
 
-function Logo({ styles, fontColor, fontSize }) {
+function Logo({ styles, fontColor, fontSize, phoneLogoSize }) {
   if (!fontColor) fontColor = "text-gray-100";
   if (!fontSize) fontSize = "text-2xl tablet:text-3xl";
   return (
     <a className={`flex flex-row items-center space-x-3 ${styles}`} href="/">
-      <img className="w-10 laptop:w-12" src={logo} alt="A pirate ship logo" />
+      <img
+        className={`${
+          phoneLogoSize ? `${phoneLogoSize} ` : ""
+        }tablet:w-10 laptop:w-12`}
+        src={logo}
+        alt="A pirate ship logo"
+      />
       <h1
         className={`${fontColor} ${fontSize} font-normal font-pirate-gr text-shadow-lg`}
       >
@@ -21,12 +27,14 @@ Logo.defaultProps = {
   styles: "",
   fontColor: "",
   fontSize: "",
+  phoneLogoSize: "",
 };
 
 Logo.propTypes = {
   styles: PropTypes.string,
   fontColor: PropTypes.string,
   fontSize: PropTypes.string,
+  phoneLogoSize: PropTypes.string,
 };
 
 export default Logo;
