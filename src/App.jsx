ee4646f7968bc6709c9bdbfad69e342a0dfb3fb6
Cristian Botez <cbotez22@gmail.com>
2021-05-05 17:31:43 +0300

    Remove the /login and /signup paths

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,15 @@ import {
   Redirect,
 } from "react-router-dom";
 import Home from "./components/Home";
-import LoginForm from "./components/LoginForm";
-import SignupForm from "./components/SignupForm";
 import PaymentForm from "./components/PaymentForm";
 import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <Router>
-      {/*TODO: redirect to payment etc to not rerender all the components? */}
       <Switch>
         <Route exact path="/">
           <Home />
-        </Route>
-        <Route path="/login">
-          <Home>
-            <LoginForm />
-          </Home>
-        </Route>
-        <Route path="/signup">
-          <Home>
-            <SignupForm />
-          </Home>
         </Route>
         <Route path="/checkout">
           <PaymentForm />
@@ -35,6 +22,8 @@ function App() {
         <Route path="/404">
           <NotFound />
         </Route>
+        <Redirect from="/login" exact to="/" />
+        <Redirect from="/signup" exact to="/" />
         <Redirect to="/404" />
       </Switch>
     </Router>
