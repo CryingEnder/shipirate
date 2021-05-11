1a9252b69a8ef90d28a444a5a8e83e7d682ec810
Cristian Botez <cbotez22@gmail.com>
2021-05-11 15:40:15 +0300

    Fix the navigating position of PlanSection

--- src/components/PlanSection.jsx
+++ src/components/PlanSection.jsx
@@ -1,48 +1,53 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Plan from "./common/Plan";
 import Container from "./common/Container";
 
 function PlanSection(props) {
   return (
-    <Container
-      tag="section"
-      stylesInside="flex-col justify-evenly items-start space-y-28 p-3 tablet:p-0 laptop:flex laptop:flex-row laptop:space-y-0 laptop:mx-auto"
-      id="plans"
-    >
-      <Plan
-        planFeatures={[
-          { available: true, feature: "Lorem ipsum dolor sit amet" },
-          { available: true, feature: "Lorem ipsum dolor sit amet" },
-          { available: false, feature: "Lorem ipsum dolor sit amet" },
-        ]}
-        numberOfMonths={1}
-        price={10}
-        currency="$"
-      />
-      <Plan
-        planFeatures={[
-          { available: true, feature: "Lorem ipsum dolor sit amet" },
-          { available: true, feature: "Lorem ipsum dolor sit amet" },
-          { available: true, feature: "Lorem ipsum dolor sit amet" },
-        ]}
-        numberOfMonths={6}
-        discountPercentage={10}
-        price={10}
-        currency="$"
-        isPopular={true}
-      />
-      <Plan
-        planFeatures={[
-          { available: true, feature: "Lorem ipsum dolor sit amet" },
-          { available: true, feature: "Lorem ipsum dolor sit amet" },
-          { available: true, feature: "Lorem ipsum dolor sit amet" },
-        ]}
-        numberOfMonths={12}
-        discountPercentage={20}
-        price={10}
-        currency="$"
-      />
-    </Container>
+    <Fragment>
+      <Container
+        tag="section"
+        stylesInside="flex-col justify-evenly items-start space-y-28 p-3 tablet:p-0 laptop:flex laptop:flex-row laptop:space-y-0 laptop:mx-auto"
+      >
+        <div
+          id="plans"
+          className="absolute bg-none w-0 h-0 -mt-36 laptop:-mt-52"
+        ></div>
+        <Plan
+          planFeatures={[
+            { available: true, feature: "Lorem ipsum dolor sit amet" },
+            { available: true, feature: "Lorem ipsum dolor sit amet" },
+            { available: false, feature: "Lorem ipsum dolor sit amet" },
+          ]}
+          numberOfMonths={1}
+          price={10}
+          currency="$"
+        />
+        <Plan
+          planFeatures={[
+            { available: true, feature: "Lorem ipsum dolor sit amet" },
+            { available: true, feature: "Lorem ipsum dolor sit amet" },
+            { available: true, feature: "Lorem ipsum dolor sit amet" },
+          ]}
+          numberOfMonths={6}
+          discountPercentage={10}
+          price={10}
+          currency="$"
+          isPopular={true}
+        />
+        <Plan
+          planFeatures={[
+            { available: true, feature: "Lorem ipsum dolor sit amet" },
+            { available: true, feature: "Lorem ipsum dolor sit amet" },
+            { available: true, feature: "Lorem ipsum dolor sit amet" },
+          ]}
+          numberOfMonths={12}
+          discountPercentage={20}
+          price={10}
+          currency="$"
+        />
+      </Container>
+    </Fragment>
   );
 }
 
