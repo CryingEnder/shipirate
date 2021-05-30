ec9a5f09b957e0d50ea78e86692df369fdd88d73
Cristian Botez <cbotez22@gmail.com>
2021-05-30 16:50:16 +0300

    Remove unused React features

--- src/components/PaymentForm.jsx
+++ src/components/PaymentForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import Container from "./common/Container";
 import Input from "./common/Input";
 import Button from "./common/Button";
