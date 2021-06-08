af3a75840a8ed08b2f4fcfcf881fda3f59234edf
Cristian Botez <cbotez22@gmail.com>
2021-06-08 14:20:41 +0300

    Change table breakpoint to laptop in Testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -69,9 +69,8 @@ function Testimonials(props) {
     },
   ];
 
-  const [userTestimonials, setUserTestimonials] = useState(
-    userTestimonialsData
-  );
+  const [userTestimonials, setUserTestimonials] =
+    useState(userTestimonialsData);
   const [currentUser, setCurrentUser] = useState(0);
   const [opacityAnimation, setOpacityAnimation] = useState("");
 
@@ -97,23 +96,25 @@ function Testimonials(props) {
 
   return (
     <Container tag="section" stylesInside="p-2">
-      <header className="text-blue-dark text-center mb-10">
+      <header className="text-center mb-16 laptop:mb-10">
         <div className="inline-block">
           <img
             className="w-12"
             src={testimonialsLogo}
             alt="A testimonial logo"
           />
-          <h2 className="text-shadow mt-2 mb-6">Testimonials</h2>
+          <h2 className="text-blue-dark dark:text-blue-whiteish-1 text-shadow mt-2 mb-6">
+            Testimonials
+          </h2>
         </div>
-        <p>
+        <p className="text-blue-dark dark:text-blue-whiteish-2">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, sint.
         </p>
       </header>
-      <div className="relative rounded-2xl mx-auto shadow-lg w-full bg-gray-25 tablet:rounded-x2 tablet:w-11/12 tablet:bg-gradient-to-br tablet:from-blue-sky-1 tablet:via-blue-sky-2 tablet:to-blue-water laptop:w-3/5">
-        <div className="px-7 pt-4 pb-14 rounded-t-2xl bg-gradient-to-br from-blue-sky-1 via-blue-sky-2 to-blue-water tablet:inline-block tablet:px-14 tablet:py-8 tablet:pb-10 tablet:bg-none tablet:bg-gray-25 tablet:mt-2 tablet:mx-2 tablet:rounded-t-x1.5 desktop:py-12">
-          <div className="absolute max-h-14 w-full flex flex-row justify-center rounded-full left-0 -top-7 tablet:top-0">
-            <div className="text-gray-25 py-2 px-4 flex flex-row justify-center space-x-6 border-6 border-solid border-gray-25 rounded-full bg-blue-sky-1 tablet:hidden">
+      <div className="relative rounded-2xl mx-auto shadow-lg w-full bg-gray-25 laptop:rounded-x2 tablet:w-11/12 laptop:bg-gradient-to-br laptop:from-blue-sky-1 laptop:via-blue-sky-2 laptop:to-blue-water laptop:w-3/5">
+        <div className="px-7 pt-4 pb-14 rounded-t-2xl bg-gradient-to-br from-blue-sky-1 via-blue-sky-2 to-blue-water laptop:inline-block laptop:px-14 laptop:py-8 laptop:pb-10 laptop:bg-none laptop:bg-gray-25 laptop:mt-2 laptop:mx-2 laptop:rounded-t-x1.5 desktop:py-12">
+          <div className="absolute max-h-14 w-full flex flex-row justify-center rounded-full left-0 -top-7 laptop:top-0">
+            <div className="text-gray-25 py-2 px-4 flex flex-row justify-center space-x-6 border-6 border-solid border-gray-25 rounded-full bg-blue-sky-1 laptop:hidden">
               <AngleLeft
                 className="w-8 fill-current cursor-pointer transform ease-out duration-200 hover:scale-110 active:scale-105"
                 onClick={getPreviousUser}
@@ -123,7 +124,7 @@ function Testimonials(props) {
                 onClick={getNextUser}
               />
             </div>
-            <div className="hidden tablet:text-blue-sky-1 tablet:pt-2 tablet:px-4 tablet:flex tablet:flex-row tablet:justify-center tablet:space-x-3 tablet:shadow-md tablet:rounded-b-x2 tablet:rounded-3xl">
+            <div className="hidden laptop:text-blue-sky-1 laptop:pt-2 laptop:px-4 laptop:flex laptop:flex-row laptop:justify-center laptop:space-x-3 laptop:shadow-md laptop:rounded-b-x2 laptop:rounded-3xl">
               <ArrowLeft
                 className="w-12 laptop:w-14 fill-current cursor-pointer transform ease-out duration-200 hover:scale-120 active:scale-110"
                 onClick={getPreviousUser}
@@ -134,9 +135,9 @@ function Testimonials(props) {
               />
             </div>
           </div>
-          <div className="text-gray-25 tablet:text-blue-dark">
+          <div className="text-gray-25 laptop:text-blue-dark">
             <DoubleQuotes
-              className={`mb-2 w-10 tablet:w-12 laptop:w-14 fill-current ${opacityAnimation}`}
+              className={`mb-2 w-10 tablet-small:w-12 laptop:w-14 fill-current ${opacityAnimation}`}
               onAnimationEnd={() => setOpacityAnimation("")}
             />
             <p
@@ -147,20 +148,20 @@ function Testimonials(props) {
             </p>
           </div>
         </div>
-        <div className="flex flex-col justify-center items-center space-y-6 px-7 pb-3 bg-transparent tablet:flex-row tablet:justify-between tablet:space-y-0 tablet:py-6 tablet:px-14">
-          <div className="max-w-xs flex flex-col items-center text-center -mt-12 tablet:max-w-full tablet:flex-row tablet:justify-between tablet:text-left tablet:mt-0 tablet:space-x-3 tablet:p-0">
+        <div className="flex flex-col justify-center items-center space-y-6 px-7 pb-3 bg-transparent laptop:flex-row laptop:justify-between laptop:space-y-0 laptop:py-6 laptop:px-14">
+          <div className="max-w-xs flex flex-col items-center text-center -mt-12 laptop:max-w-full laptop:flex-row laptop:justify-between laptop:text-left laptop:mt-0 laptop:space-x-3 laptop:p-0">
             <div
               className={
-                "rounded-full box-content relative bg-gray-25 p-2.5 min-w-max tablet:p-1.5"
+                "rounded-full box-content relative bg-gray-25 p-2.5 min-w-max laptop:p-1.5"
               }
             >
               <img
-                className="rounded-full w-20 tablet:w-24 desktop:w-28"
+                className="rounded-full w-20 laptop:w-24 desktop:w-28"
                 src={userTestimonials[currentUser].profilePhoto}
                 alt="A profile photo"
               />
               {userTestimonials[currentUser].socialPlatform && (
-                <div className="bg-gray-25 p-0.5 rounded-full absolute right-0.5 bottom-1 w-8 tablet:hidden">
+                <div className="bg-gray-25 p-0.5 rounded-full absolute right-0.5 bottom-1 w-8 laptop:hidden">
                   <img
                     src={userTestimonials[currentUser].socialPlatform}
                     alt="Social media icon"
@@ -168,8 +169,8 @@ function Testimonials(props) {
                 </div>
               )}
             </div>
-            <div className="tablet:max-w-xs">
-              <p className="text-blue-dark tablet:text-gray-25 font-bold">
+            <div className="laptop:max-w-xs">
+              <p className="text-blue-dark laptop:text-gray-25 font-bold">
                 {userTestimonials[currentUser].name}
               </p>
               <p className="text-gray-bluegray-500 font-semibold">
@@ -179,7 +180,7 @@ function Testimonials(props) {
           </div>
           {userTestimonials[currentUser].socialPlatform && (
             <img
-              className={"hidden tablet:block tablet:w-16"}
+              className={"hidden laptop:block laptop:w-16"}
               src={userTestimonials[currentUser].socialPlatform}
               alt="Social media icon"
             />
