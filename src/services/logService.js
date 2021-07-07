aa31dca6415abe4612d99fe1cbbe3ec1deab380f
Cristian Botez <cbotez22@gmail.com>
2021-07-07 13:48:20 +0300

    Condition the log service based on env variables

--- src/services/logService.js
+++ src/services/logService.js
@@ -5,7 +5,7 @@ function init() {
   Sentry.init({
     dsn: "https://139fc37b8be34b828ca9359634f0f4cb@o452961.ingest.sentry.io/5672793",
     integrations: [new Integrations.BrowserTracing()],
-    tracesSampleRate: 1.0, //to be adjusted in production
+    tracesSampleRate: import.meta.env.DEV ? 1.0 : 0.5, //to be adjusted in production
   });
 }
 
