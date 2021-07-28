b6398e842f35a18f31c202c8cc522067ad2508ba
Cristian Botez <cbotez22@gmail.com>
2021-07-28 18:26:56 +0300

    Make code easier to read in user.js and db.js

--- api/models/user.js
+++ api/models/user.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const Joi = require('joi');
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const Joi = require("joi");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -16,32 +16,40 @@ const userSchema = new mongoose.Schema({
     minlength: 10,
     maxlength: 255,
     lowercase: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
     required: true,
     minlength: 5,
-    maxlength: 1024
+    maxlength: 1024,
   },
-  isAdmin: Boolean
-}); 
+  isAdmin: Boolean,
+});
 
-userSchema.methods.generateAuthToken = function() { 
-  const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'));
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    config.get("jwtPrivateKey")
+  );
   return token;
-}
+};
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
-  const schema = Joi.object().keys({
-    username: Joi.string().alphanum().min(5).max(30).required(),
-    email: Joi.string().email().min(10).max(255).required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).min(5).max(1024).required(),
-    repeatPassword: Joi.ref('password')
-  })
-  .with('password', 'repeatPassword');
+  const schema = Joi.object()
+    .keys({
+      username: Joi.string().alphanum().min(5).max(30).required(),
+      email: Joi.string().email().min(10).max(255).required(),
+      password: Joi.string()
+        .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+        .min(5)
+        .max(1024)
+        .required(),
+      repeatPassword: Joi.ref("password"),
+    })
+    .with("password", "repeatPassword");
 
   return schema.validate(user);
 }
