6f38f9f6f8d11dca8b260d2ea42e2e1cf04a2e5e
Cristian Botez <cbotez22@gmail.com>
2021-08-01 17:27:57 +0300

    Add config and CORS on startup

--- api/index.js
+++ api/index.js
@@ -1,13 +1,17 @@
-const winston = require('winston');
-const express = require('express');
+const winston = require("winston");
+const express = require("express");
 const app = express();
 
-require('./startup/logging')();
-require('./startup/routes')(app);
-require('./startup/db')();
+require("./startup/logging")();
+require("./startup/cors")(app);
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
 
 const port = process.env.PORT || 3000;
 
-const server = app.listen(port, () => { winston.info(`Connected to port ${port}`) });
+const server = app.listen(port, () => {
+  winston.info(`Connected to port ${port}`);
+});
 
 module.exports = server;
