6c6fb321c907e7f557ca6113adb46f34e1b360bf
Cristian Botez <cbotez22@gmail.com>
2021-08-02 19:22:16 +0300

    Store server response into an object in auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,27 +1,39 @@
 //Authentication
-const { User } = require('../models/user'); // = x.User and x.validate
-const tryCatch = require('../middleware/async'); 
-const validator = require('../middleware/validate');
-const Joi = require('joi');
-const bcrypt = require('bcrypt');
-const express = require('express');
+const { User } = require("../models/user"); // = x.User and x.validate
+const tryCatch = require("../middleware/async");
+const validator = require("../middleware/validate");
+const Joi = require("joi");
+const bcrypt = require("bcrypt");
+const express = require("express");
 const router = express.Router();
 
-router.post('/', validator(validate), tryCatch(async (req, res) => {
-  let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send('Invalid e-mail.');
+router.post(
+  "/",
+  validator(validate),
+  tryCatch(async (req, res) => {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send({ email: "Invalid e-mail." });
 
-  const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword) return res.status(400).send('Invalid password.');
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword)
+      return res.status(400).send({ password: "Invalid password." });
 
-  const token = user.generateAuthToken();
-  res.send({token});
-}));
+    const token = user.generateAuthToken();
+    res.send(token);
+  })
+);
 
 function validate(req) {
   const schema = Joi.object().keys({
     email: Joi.string().email().min(10).max(255).required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).min(5).max(1024).required()
+    password: Joi.string()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .min(5)
+      .max(1024)
+      .required(),
   });
 
   return schema.validate(req);
