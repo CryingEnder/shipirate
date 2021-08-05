42beb83eefd30d6fe4e15a9a37b2b0a526bdeea0
Cristian Botez <cbotez22@gmail.com>
2021-08-05 20:30:09 +0300

    Rework Input component

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { ThemeContext } from "./../context/ThemeContext";
 import PropTypes from "prop-types";
 
-function Input({ label, styles, linkPath, ...props }) {
+function Input({ label, styles, alignLeft, linkPath, ...props }) {
   const { theme, setTheme } = useContext(ThemeContext);
 
   //error message TODO:
@@ -31,66 +31,65 @@ function Input({ label, styles, linkPath, ...props }) {
   }
 
   return (
-    <div
-      className={`relative flex flex-row justify-start items-center${
-        styles ? ` ${styles}` : ""
-      }`}
-    >
+    <Fragment>
       {props.type !== "checkbox" && props.type !== "radio" && (
-        <div className="max-w-min">
+        <div className={`w-full relative${styles ? ` ${styles}` : ""}`}>
           <input
             onChange={handleChange}
             onFocus={toggleTransform}
             onBlur={toggleTransform}
-            className={`outline-none text-sm tablet:text-lg rounded-lg p-3 tablet:p-4 text-blue-dark focus:bg-gray-100 placeholder-blue-grayish bg-gray-25`}
+            className={`outline-none w-full text-sm tablet:text-lg rounded-lg p-3 tablet:p-4 text-blue-dark focus:bg-gray-100 placeholder-blue-grayish bg-gray-25`}
             name={props.name}
             {...props}
           />
           {props.error && (
-            <div className="relative flex flex-row justify-start items-center mt-3 w-full h-auto select-none rounded-lg font-bold text-shadow border-3 border-solid border-yellow-pirate-ring bg-yellow-pirate text-blue-dark dark:bg-gray-25 dark:border-gray-bluegray-300 dark:text-red-sky-error tablet:w-full tablet:absolute tablet:top-0 tablet:bottom-0 tablet:my-auto tablet:ml-6 tablet:rounded-xl tablet:left-full tablet:h-4/5">
+            <div className="relative flex flex-row justify-start items-center mt-3 w-full h-auto select-none rounded-lg font-bold text-shadow border-3 border-solid border-yellow-pirate-ring bg-yellow-pirate text-blue-dark dark:bg-gray-25 dark:border-gray-bluegray-300 dark:text-red-sky-error tablet:w-11/12 tablet:absolute tablet:top-0 tablet:bottom-0 tablet:my-auto tablet:ml-6 tablet:rounded-xl tablet:left-full tablet:h-5/6">
               <div className="absolute left-4 -top-3 w-3 h-3 clip-top-triangle bg-yellow-pirate-ring dark:bg-gray-bluegray-300 tablet:top-auto tablet:-left-4 tablet:w-4 tablet:h-4 tablet:clip-left-triangle" />
               <p className="text-xs mx-2 my-1 tablet:m-4">{props.error}</p>
             </div>
           )}
-        </div>
-      )}
-      {(props.type === "checkbox" || props.type === "radio") && (
-        <input
-          {...props}
-          className={`appearance-none cursor-pointer outline-none w-8 h-8 tablet:w-10 tablet:h-10 -ml-1 rounded-lg bg-no-repeat ${
-            theme !== "dark"
-              ? "bg-checkbox-unchecked checked:bg-checkbox-checked"
-              : "bg-checkbox-unchecked-dark checked:bg-checkbox-checked-dark"
-          }`}
-        />
-      )}
-      {props.type === "checkbox" || props.type === "radio" ? (
-        linkPath ? (
           <label
-            className="cursor-pointer text-base tablet:text-lg pl-2 hover:underline text-blue-dark dark:text-purple-light-3"
-            htmlFor={props.id}
-          >
-            <Link target="_blank" to={linkPath}>
-              {label}
-            </Link>
-          </label>
-        ) : (
-          <label
-            className="cursor-pointer text-base tablet:text-lg font-semibold pl-2 text-blue-dark dark:text-purple-light-3"
+            className={`top-3 tablet:top-4.5 absolute cursor-text block ml-4 text-blue-dark ${labelTransformClass}`}
             htmlFor={props.id}
           >
             {label}
           </label>
-        )
-      ) : (
-        <label
-          className={`top-3.5 tablet:top-4.5 absolute block cursor-text ml-4 text-blue-dark ${labelTransformClass}`}
-          htmlFor={props.id}
-        >
-          {label}
-        </label>
+        </div>
       )}
-    </div>
+      {(props.type === "checkbox" || props.type === "radio") && (
+        <div
+          className={`w-full flex flex-row items-center ${
+            alignLeft ? "justify-start" : "justify-center"
+          }${styles ? ` ${styles}` : ""}`}
+        >
+          <input
+            {...props}
+            className={`appearance-none cursor-pointer outline-none w-8 h-8 tablet:w-10 tablet:h-10 -ml-1 rounded-lg bg-no-repeat ${
+              theme !== "dark"
+                ? "bg-checkbox-unchecked checked:bg-checkbox-checked"
+                : "bg-checkbox-unchecked-dark checked:bg-checkbox-checked-dark"
+            }`}
+          />
+          {linkPath ? (
+            <label
+              className="cursor-pointer text-center text-base tablet:text-lg pl-2 hover:underline text-blue-dark dark:text-purple-light-3"
+              htmlFor={props.id}
+            >
+              <Link target="_blank" to={linkPath}>
+                {label}
+              </Link>
+            </label>
+          ) : (
+            <label
+              className="cursor-pointer text-base tablet:text-lg font-semibold pl-2 text-blue-dark dark:text-purple-light-3"
+              htmlFor={props.id}
+            >
+              {label}
+            </label>
+          )}
+        </div>
+      )}
+    </Fragment>
   );
 }
 
@@ -98,6 +97,7 @@ Input.defaultProps = {
   label: "",
   linkPath: "",
   styles: "",
+  alignLeft: false,
 };
 
 Input.propTypes = {
@@ -105,6 +105,7 @@ Input.propTypes = {
   label: PropTypes.string,
   linkPath: PropTypes.string,
   styles: PropTypes.string,
+  alignLeft: PropTypes.bool,
 };
 
 export default Input;
