a41061cd2f2344c0bf6db51f2f70d0e89f8ec53d
Cristian Botez <cbotez22@gmail.com>
2021-08-09 20:53:18 +0300

    Use cookie-parser in the api

--- api/startup/routes.js
+++ api/startup/routes.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const users = require('../routes/users');
-const auth = require('../routes/auth');
-// const members = require('../routes/members');
+const express = require("express");
+const users = require("../routes/users");
+const auth = require("../routes/auth");
+const cookieParser = require("cookie-parser");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(express.json());
-  app.use('/api/users', users);
-  app.use('/api/auth', auth);
-  // app.use('/api/members', members);
-}
+  app.use(cookieParser());
+  app.use("/api/users", users);
+  app.use("/api/auth", auth);
+};
