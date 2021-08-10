1ffa8df7ecde49bd34fea3c44b749e8f36434255
Cristian Botez <cbotez22@gmail.com>
2021-08-11 00:11:58 +0300

    Update the error logging format

--- api/startup/logging.js
+++ api/startup/logging.js
@@ -1,21 +1,20 @@
-const winston = require('winston');
+const winston = require("winston");
 //TODO: logging errors in the database
 
-module.exports = function() {
+module.exports = function () {
   winston.configure({
+    format: winston.format.simple(),
     transports: [
       new winston.transports.Console(),
-      new winston.transports.File({ filename: 'combined.log' }) 
+      new winston.transports.File({ filename: "combined.log" }),
     ],
     exceptionHandlers: [
       new winston.transports.Console(),
-      new winston.transports.File({ filename: 'exceptions.log' }) 
+      new winston.transports.File({ filename: "exceptions.log" }),
     ],
     rejectionHandlers: [
       new winston.transports.Console(),
-      new winston.transports.File({ filename: 'rejections.log' })
-    ]
-  })
-}
-
-  
+      new winston.transports.File({ filename: "rejections.log" }),
+    ],
+  });
+};
