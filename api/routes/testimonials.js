3075afc3a4570d3ace25e1ebfe5e327224e3bd46
Cristian Botez <cbotez22@gmail.com>
2021-08-16 01:10:31 +0300

    Add testimonials route

---
+++ api/routes/testimonials.js
@@ -0,0 +1,44 @@
+const { Testimonial, validate } = require("../models/testimonial");
+const auth = require("../middleware/auth");
+const isAdmin = require("../middleware/isAdmin");
+const upload = require("../middleware/upload");
+const tryCatch = require("../middleware/async");
+const { unlink } = require("fs/promises");
+
+const express = require("express");
+const router = express.Router();
+
+router.get(
+  "/",
+  tryCatch(async (req, res) => {
+    const testimonials = await Testimonial.find().select("-__v").sort("name");
+    res.send(testimonials);
+  })
+);
+
+router.post(
+  "/",
+  [auth, isAdmin, upload.single("profilePhoto")],
+  tryCatch(async (req, res) => {
+    const { error } = validate(req.body);
+    const path = `public/images/${req.file.filename}`;
+    if (error) {
+      await unlink(path);
+      return res.status(400).send(error.details[0].message);
+    }
+
+    testimonial = new Testimonial({
+      profilePhoto: path,
+      name: req.body.name,
+      profession: req.body.profession,
+      socialPlatform: req.body.socialPlatform,
+      message: req.body.message,
+    });
+
+    await testimonial.save();
+
+    res.status(200).send(testimonial);
+  })
+);
+
+module.exports = router;
--- api/startup/routes.js
+++ api/startup/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
 const plans = require("../routes/plans");
+const testimonials = require("../routes/testimonials");
 const cookieParser = require("cookie-parser");
 
 module.exports = function (app) {
@@ -10,4 +11,5 @@ module.exports = function (app) {
   app.use("/api/users", users);
   app.use("/api/auth", auth);
   app.use("/api/plans", plans);
+  app.use("/api/testimonials", testimonials);
 };
