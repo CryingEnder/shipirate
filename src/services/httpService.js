8d71c587720152c6475b202a9922fd19d63bce76
Cristian Botez <cbotez22@gmail.com>
2021-08-16 01:03:03 +0300

    Remove comments

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -3,18 +3,6 @@ import logger from "./logService";
 
 axios.defaults.withCredentials = true;
 axios.interceptors.response.use(null, (error) => {
-  /*
-    Great article, thanks! I learnt a lot. I believe that once the token starts coming back in a cookie, we no longer need the `axios.interceptors.request.use`.
-    It makes sense since this is adding a header for the JWT to be "fished" backend side but since the article didn't mention it,
-    I didn't realise, and keptgetting errors when making the api call. I thought it was the fault of the proxy, but it was not!
-  */
-
-  /*
-    Also, the express-jwt has a breaking change in the latest version. You need to provide which algorithm to use, otherwise an error will be thrown.
-    For ex:
-    expressJwt({ secret: process.env.JWT_SECRET, algorithms: ['RS256'] });
-  */
-  //(fulfilled, rejected)
   const expectedError =
     error.response &&
     error.response.status >= 400 &&
