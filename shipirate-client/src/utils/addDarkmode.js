46e60a195b8d7213c3ebf175b0e8ee9162109401
Cristian Botez <cbotez22@gmail.com>
2021-08-20 19:22:00 +0300

    Remove unused utils

--- shipirate-client/src/utils/addDarkmode.js
+++
@@ -1,8 +0,0 @@
-export function addDarkmode(classes) {
-  if (typeof classes === "string" && classes) {
-    classes = ` ${classes}`;
-    let newClasses = classes.replace(/\s/g, ` dark:`).replace(" ", "");
-    return newClasses;
-  }
-  throw new Error();
-}
