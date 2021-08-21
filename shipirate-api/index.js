a4a974ce0cf816c8d78d4e1f7f7232921a1023ec
Cristian Botez <cbotez22@gmail.com>
2021-08-21 18:25:09 +0300

    Prepare API for production

--- shipirate-api/index.js
+++ shipirate-api/index.js
@@ -8,6 +8,8 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 
+if (process.env.NODE_ENV === "production") require("./startup/prod")(app);
+
 const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
